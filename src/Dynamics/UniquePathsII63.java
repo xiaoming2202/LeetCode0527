8afcfc1292fa12cbef5694cbde982a6354e140c7
yanmei <yanmei@tsign.cn>
2019-05-31 00:18:03 +0800

    不同路径2最终版

--- src/Dynamics/UniquePathsII63.java
+++ src/Dynamics/UniquePathsII63.java
@@ -33,24 +33,33 @@ public class UniquePathsII63 {
 		int[][] path = new int[obstacleGrid.length][obstacleGrid[0].length];
 		for (int i = 0; i < hang; i++) {
 			for (int j = 0; j < lie; j++) {
-				if (i == 0 || j == 0)
-					path[i][j] = 1;
-				else {
-					path[i][j] = obstacleGrid[i - 1][j] == 0 ? path[i - 1][j]
-							: 0 + obstacleGrid[i][j - 1] == 0 ? path[i][j - 1] : 0;
+				if (obstacleGrid[i][j] == 1) {
+					path[i][j] = 0;
+				} else if (i == 0 || j == 0) {
+					if (i == 0 && j == 0)
+						path[i][j] = 1;
+					else if (i == 0)
+						path[i][j] = path[i][j - 1];
+					else if (j == 0)
+						path[i][j] = path[i - 1][j];
+				} else {
+					int add1 = (obstacleGrid[i - 1][j] == 0) ? path[i - 1][j] : 0;
+					int add2 = (obstacleGrid[i][j - 1] == 0) ? path[i][j - 1] : 0;
+					path[i][j] = add1 + add2;
 				}
 			}
 		}
+		return path[hang - 1][lie - 1];
+	}
 
-		return path[hang-1][lie-1];
-	}
-	public static  void main(String []args) {
-		int [][]input= {
-				{0,0,0},
-				{0,1,0},
-				{0,0,0}
-		};
-		int res=new UniquePathsII63().uniquePathsWithObstacles(input);
+	public static void main(String[] args) {
+		/*
+		 * int [][]input= { {0,0,0}, {0,1,0}, {0,0,0} };
+		 */
+		int[][] input = { { 1 } };
+		int res = new UniquePathsII63().uniquePathsWithObstacles(input);
 		System.out.println(res);
+
 	}
+
 }
